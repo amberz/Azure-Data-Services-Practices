4b1be303b9035fda0e56f3d832f4af62f6b081e9
Amber Zhao <amberzhao01@hotmail.com>
2020-10-30 15:25:49 -0700

    add temp table size

--- Data modernization/GetSQLDBTableSize.sql
+++ Data modernization/GetSQLDBTableSize.sql
@@ -28,3 +28,17 @@ GROUP BY
     t.Name, s.Name, p.Rows
 ORDER BY 
     TotalSpaceMB DESC, t.Name
+
+-- get temp table size
+SELECT TBL.name AS ObjName 
+      ,STAT.row_count AS StatRowCount 
+      ,STAT.used_page_count * 8 AS UsedSizeKB 
+      ,STAT.reserved_page_count * 8 AS RevervedSizeKB 
+FROM tempdb.sys.partitions AS PART 
+     INNER JOIN tempdb.sys.dm_db_partition_stats AS STAT 
+         ON PART.partition_id = STAT.partition_id 
+            AND PART.partition_number = STAT.partition_number 
+     INNER JOIN tempdb.sys.tables AS TBL 
+         ON STAT.object_id = TBL.object_id 
+ORDER BY TBL.name;
+
